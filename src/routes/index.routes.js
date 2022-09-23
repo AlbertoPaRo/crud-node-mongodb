4592ba51c504156d899c8eb1d37780069c0ebefe
AlbertoPaRo <albertopadillaroj@gmail.com>
2022-09-22 23:38:39 -0400

    button-update

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,20 +1,34 @@
 const express = require("express");
+const Task = require("../models/Task");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  const tasks = await Task.find().lean();
+  res.render("index", { tasks: tasks });
 });
-router.post("/task/add", (req, res) => {
-  console.log(req.body);
-  res.send("saved");
+
+router.post("/task/add", async (req, res) => {
+  try {
+    const task = Task(req.body);
+    await task.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
+
 router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/edit", (req, res) => {
+router.get("/edit/:id", (req, res) => {
   res.render("edit");
+});
+
+router.post("/edit/:id", (req, res) => {
+  console.log(req.body);
+  res.send("received");
 });
 
 module.exports = { router: router };
