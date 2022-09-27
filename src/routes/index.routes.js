bd1ff96f399bcf70bf7c8eee053da2ec40f8e8a8
AlbertoPaRo <albertopadillaroj@gmail.com>
2022-09-26 22:50:40 -0400

    delete-update

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -22,13 +22,32 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/edit/:id", (req, res) => {
-  res.render("edit");
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+    res.render("edit", { task });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post("/edit/:id", (req, res) => {
-  console.log(req.body);
-  res.send("received");
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndUpdate(id, req.body);
+  res.redirect("/");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
+router.get("/toggledone/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndUpdate(id);
 });
 
 module.exports = { router: router };
